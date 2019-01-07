f31376b8cfed5199db74fd4d47035b6c4553b965
rudolphwen <rudolph.weng@gmail.com>
2019-01-07 10:42:38 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -37,10 +37,30 @@ io.on('connection', (socket) => {
     //     console.log('createEmail', newEmail);
     // });
 
+    // socket.emit from Admin text Welcome to the chat app
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
